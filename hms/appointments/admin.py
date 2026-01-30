f8b99289307fdaf81acb018eecfd8530dfe06110
annnnishthapa <galaxyanish123@gmail.com>
2026-01-30 13:21:36 +0545

    admin for model.py

--- hms/appointments/admin.py
+++ hms/appointments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import appointment
 
-# Register your models here.
+@admin.register(appointment)
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'doctor', 'appointment_date', 'status')
+    search_fields = ('patient__user__username', 'doctor__user__username')
+    list_filter = ('status', 'priority', 'appointment_date')
+    readonly_fields = ('created_at', 'updated_at')
